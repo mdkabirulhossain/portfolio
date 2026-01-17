e0595fa7ac53b5939c96c3d7e5b7a90902eaed04
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-01-17 17:42:13 +0600

    Added padding top

--- app/(site)/components/Hero.tsx
+++ app/(site)/components/Hero.tsx
@@ -388,7 +388,7 @@ const HeroSection = () => {
             </div>
 
             {/* Main Content */}
-            <div className="relative z-10 flex items-center justify-center min-h-screen pt-40 sm:pt-0 pb-16 sm:pb-0 px-4 sm:px-6 lg:px-8">
+            <div className="relative z-10 flex items-center justify-center min-h-screen pt-40  pb-16 sm:pb-0 px-4 sm:px-6 lg:px-8">
                 <div className="w-full max-w-7xl mx-auto">
                     <div className="flex flex-col-reverse gap-12 lg:gap-16">
                         {/* Left Content */}
